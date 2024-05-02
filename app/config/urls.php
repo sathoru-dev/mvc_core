4e8a2f416f078959fb488780ca59bc44802c276a
JoynerDev <joynerperezdev@gmail.com>
2024-05-01 21:10:28 -0400

    registro de rutas de pruebas

--- app/config/urls.php
+++ app/config/urls.php
@@ -1,3 +1,4 @@
 <?php
-    $this->new_url('', 'Lost->ssads', 'sas');
-    $this->new_url('prueba/?int/na/?str', 'Lost->ssads', 'sas');
+    $this->new_url('', 'Lost@ssads', 'sas');
+    $this->new_url('prueba/?str/string/?int', 'Lost->ssads', 'busquedaCliente');
+    $this->new_url('prueba/?int/string/?str', 'Lost->ssads', 'pruebaHolaMundo');
